c8ffc3d28cb81347059bb9c3a2fbb1aa6d5496d8
jcshin <shinjc28@gmail.com>
2021-09-12 13:35:15 +0900

    JDBCUtility 추가

---
+++ src/main/java/com/mes/db/JDBCUtility.java
@@ -0,0 +1,89 @@
+package com.mes.db;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class JDBCUtility {
+
+	public static Connection getConnection() {
+		Connection conn = null;
+		
+		try {
+			Context initCtx = new InitialContext();
+			Context envCtx = (Context) initCtx.lookup("java:comp/env");
+			DataSource ds = (DataSource) envCtx.lookup("jdbc/Bookrental");
+			conn = ds.getConnection();
+			conn.setAutoCommit(false);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("DB 접속에 실패했습니다.");
+		}
+		return conn;
+	}
+	
+	public static void close(Connection conn) {
+		try {
+			if(conn != null) conn.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void close(Statement stmt,  ResultSet rs) {
+		try {
+			if(rs != null) rs.close();
+			if(stmt != null) stmt.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void close(Statement stmt) {
+		try {
+			if(stmt != null) stmt.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void close(Connection conn, Statement stmt) {
+		try {
+			if(stmt != null) stmt.close();
+			if(conn != null) conn.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void close(Connection conn, Statement stmt, ResultSet rs) {
+		try {
+			if(rs != null)   rs.close();
+			if(stmt != null) stmt.close();
+			if(conn != null) conn.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void commit(Connection conn) {
+		try {
+			conn.commit();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void rollback(Connection conn) {
+		try {
+			conn.rollback();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
